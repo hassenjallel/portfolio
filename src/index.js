1867013e979c159aec601f1a3c9e67510860780d
hassenjallel <hassen.jalleli@esprit.tn>
2022-02-18 13:09:03 +0000

    last one

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={"hassen-portfolio"}>
     <App />
     </Router>
   </React.StrictMode>,
